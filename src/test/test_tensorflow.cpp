7fb9a952e49f93988b2e9509618a59f536702d85
cdsunxiaoxiong <cdsunxiaoxiong@jd.com>
2019-07-23 01:28:50 -0700

    add third party file

--- src/test/test_tensorflow.cpp
+++ src/test/test_tensorflow.cpp
@@ -23,6 +23,9 @@ int test_create_session();
 //test load cnn tensorflow model
 int test_load_cnn_classifier_model();
 
+//test load bi lstm crf model
+int test_load_bi_lstm_crf_model();
+
 int main() 
 {
     test_version();
@@ -30,6 +33,8 @@ int main()
     test_create_session();
     
     test_load_cnn_classifier_model();
+
+    test_load_bi_lstm_crf_model();
 
     return 0;
 }
@@ -106,6 +111,71 @@ int test_load_cnn_classifier_model()
     //test cout result tensor
     for (int i = 0; i < count; ++i) {
         const float value = prediction(i);
+        std::cout << i << ":" << value << std::endl;
+        // value是该张量以一维数组表示时在索引i处的值。
+    }
+
+	return 0;
+}
+
+int test_load_bi_lstm_crf_model()
+{
+    std::unique_ptr<tf::TfModel> model_;
+	
+    model_.reset(new tf::TfModel());
+    
+    std::string model_path = "./kcws/models/alerts_biLSTM_crf.pb";
+	if (!model_->Load(model_path)) {
+		std::cout << "Could not load model from: " << model_path << std::endl;
+		return false;
+	}
+
+    //create input tensor
+	std::vector<tensorflow::Tensor> trans_tensors;
+    
+    //test load other tensorflow model
+    std::vector<std::string> output_names({"decode_tags"});
+    
+    std::vector<std::pair<std::string, tensorflow::Tensor> > inputTensors;
+
+    //intput word_ids tensor
+    std::initializer_list<int> v_a = {17, 48, 211, 404, 2170, 2560, 4, 3141, 682};
+    tensorflow::Tensor tensor_in_a(tensorflow::DT_INT32, tensorflow::TensorShape({1, 9}));
+    tensor_in_a.matrix<int>().setValues({v_a});
+    inputTensors.push_back(std::pair<std::string, tensorflow::Tensor>("word_ids", tensor_in_a)); 
+    //std::vector<int> x = {17, 48, 211, 404, 2170, 2560, 4, 3141, 682};
+    //int ndim = x.size();
+    //tensorflow::Tensor input_x(tensorflow::DT_INT32, tensorflow::TensorShape({1, 9}));
+    //auto x_map = input_x.tensor<int, 2>();
+    //for (int j = 0; j < ndim; j++) {
+    //    x_map(0, j) = x[j];
+    //}
+    //inputTensors.push_back(std::pair<std::string, tensorflow::Tensor>("word_ids", input_x)); 
+    
+    //input dropout
+    tensorflow::Tensor dropout(tensorflow::DT_FLOAT, tensorflow::TensorShape({1}));
+    dropout.vec<float>()(0) = 1.0f;
+    inputTensors.push_back(std::pair<std::string, tensorflow::Tensor>("dropout", dropout));
+
+    //input sequence_lengths
+    tensorflow::Tensor sequence_lengths(tensorflow::DT_INT32, tensorflow::TensorShape({1}));
+    sequence_lengths.vec<int>()(0) = 9;
+    inputTensors.push_back(std::pair<std::string, tensorflow::Tensor>("sequence_lengths", sequence_lengths));
+
+	if (!model_->Eval(inputTensors, output_names, trans_tensors)) {
+		std::cout << "Error during get trans tensors: " << std::endl;
+		return false;
+	}
+
+    //get output res
+    std::cout<< "Reading from layer:" << output_names[0] << std::endl;
+	tensorflow::Tensor* output = &trans_tensors[0];
+	const Eigen::TensorMap<Eigen::Tensor<int, 1, Eigen::RowMajor>, Eigen::Aligned>& prediction = output->flat<int>();
+	const int count = prediction.size();
+
+    //test cout result tensor
+    for (int i = 0; i < count; ++i) {
+        const int value = prediction(i);
         std::cout << i << ":" << value << std::endl;
         // value是该张量以一维数组表示时在索引i处的值。
     }
